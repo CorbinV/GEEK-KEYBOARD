soybeanjs/soybean-admin

feat(projects): Add prefix to local storage

src/utils/storage.ts
@@ -1,7 +1,9 @@
 import { createLocalforage, createStorage } from '@sa/utils';
 
-export const localStg = createStorage<StorageType.Local>('local');
+const StoragePrefix = import.meta.env.VITE_STORAGE_PREFIX || '';
 
-export const sessionStg = createStorage<StorageType.Session>('session');
+export const localStg = createStorage<StorageType.Local>('local', StoragePrefix);
+
+export const sessionStg = createStorage<StorageType.Session>('session', StoragePrefix);
 
 export const localforage = createLocalforage<StorageType.Local>('local');

CorbinV/GEEK-KEYBOARD

src/utils/storage.ts
@@ -1,7 +1,9 @@
 import { createLocalforage, createStorage } from '@sa/utils';
 
-export const localStg = createStorage<StorageType.Local>('local');
+const storagePrefix = import.meta.env.VITE_STORAGE_PREFIX || '';
 
-export const sessionStg = createStorage<StorageType.Session>('session');
+export const localStg = createStorage<StorageType.Local>('local', storagePrefix);
+
+export const sessionStg = createStorage<StorageType.Session>('session', storagePrefix);
 
 export const localforage = createLocalforage<StorageType.Local>('local');