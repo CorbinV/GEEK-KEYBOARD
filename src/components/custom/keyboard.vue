762e4ded2a0e2aa62d0b69f10534ff92e402da09
CorbinV <vcorbin.wynn@gmail.com>
2024-11-09 16:28:22 +0800

    feat: 动态键盘基础挂载

---
+++ src/components/custom/keyboard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed, toRefs } from 'vue';
+import { useKeyboardStore } from '@/store/modules/keyboard';
+import KeyboardKey from './keyboard-key.vue';
+
+const keyboardStore = useKeyboardStore();
+const { kbCfg } = toRefs(keyboardStore);
+
+const layoutList = computed(() => {
+  return Object.keys(kbCfg.value.data).filter((k: string) => k !== 'base');
+});
+
+function handleKeyClick(e) {
+  const targetElement = e.target.closest('[data-id]');
+  if (targetElement) {
+    // do something
+    // console.log(targetElement.dataset.id);
+  }
+}
+// feat: support keyboard change
+// type KeyboardProps = {
+//   color?: string;
+//   module?: string;
+// };
+// withDefaults(defineProps<KeyboardProps>(), {
+//   module: 'rk-s75'
+// });
+</script>
+
+<template>
+  <div class="relative bg-#16161d" @click="handleKeyClick">
+    <KeyboardKey v-for="key in layoutList" :key="key" :key-id="key" />
+  </div>
+</template>
