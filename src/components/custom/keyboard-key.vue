34aa54bfd3d9eb2c6c04ca740cf73bfd6c32a2d7
CorbinV <vcorbin.wynn@gmail.com>
2024-11-12 11:52:03 +0800

    feat: 动态键盘键值组件动态支持图标以及混合字符渲染

--- src/components/custom/keyboard-key.vue
+++ src/components/custom/keyboard-key.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-import { onMounted, ref, toRefs } from 'vue';
+import { onMounted, reactive, ref, toRefs, watchEffect } from 'vue';
 import { useKeyboardStore } from '@/store/modules/keyboard';
+import type { KeyCfg } from '@/api/modules/keyboard';
+
 interface KeyboardKeyProps {
   keyId: string;
   selected?: boolean;
+  keyDetail?: any;
 }
 const props = defineProps<KeyboardKeyProps>();
 const keyInfo = ref();
 const keyStyle = ref({});
-function useLayout(kbCfg) {
+function useLayout(kbCfg: any) {
   keyInfo.value = kbCfg.value.data?.[props.keyId];
   const base = kbCfg.value.data?.base;
   if (keyInfo.value) {
@@ -31,10 +34,29 @@ function useLayout(kbCfg) {
     kbCfg.value.offsetList[row] = offset + kw + base.gap;
   }
 }
+const KeyView = reactive({
+  label: props.keyId,
+  icon: '',
+  type: 'str'
+});
 onMounted(async () => {
   const keyboardStore = useKeyboardStore();
   const { kbCfg } = toRefs(keyboardStore);
   useLayout(kbCfg);
+  function updateKeyCfg(data: KeyCfg) {
+    const { code, type } = data;
+    const info = kbCfg.value.keyMap[type]?.code?.[code];
+    if (info) {
+      KeyView.label = info.label;
+      KeyView.icon = info.icon;
+      KeyView.type = info.type;
+    }
+  }
+  watchEffect(() => {
+    if (props.keyDetail) {
+      updateKeyCfg(props.keyDetail);
+    }
+  });
 });
 
 const isLightColor = ['W', 'A', 'S', 'D', 'UP', 'DOWN', 'LEFT', 'RIGHT'].includes(props.keyId);
@@ -53,9 +75,22 @@ const isLightColor = ['W', 'A', 'S', 'D', 'UP', 'DOWN', 'LEFT', 'RIGHT'].include
         ]"
         :data-id="keyId"
       >
-        {{ keyId }}
+        <div class="h-full w-full flex flex-col items-center justify-center break-words">
+          <template v-if="KeyView.type === 'mix'">
+            <span class="inline-flex flex-row items-center justify-center">
+              <i class="iconfont" :class="`icon-${KeyView.icon}`"></i>
+              {{ KeyView.label }}
+            </span>
+          </template>
+          <template v-else-if="KeyView.type === 'icon'">
+            <i class="iconfont" :class="`icon-${KeyView.icon}`"></i>
+          </template>
+          <template v-else>
+            <span class="break-words text-center">{{ KeyView.label }}</span>
+          </template>
+        </div>
       </div>
     </template>
-    <!--TODO: KEY DESCRIPTION -->
+    <!--feat: KEY DESCRIPTION -->
   </NTooltip>
 </template>
