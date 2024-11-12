d8e2a2dd4973468ca4de32b2287648194d10c29f
CorbinV <vcorbin.wynn@gmail.com>
2024-11-12 16:04:47 +0800

    feat(example): 新增键值模块请求示例

--- src/api/modules/keyboard.ts
+++ src/api/modules/keyboard.ts
@@ -6,3 +6,19 @@ export type KeyCfg = {
 export type KeyCfgMap = {
   [key: string]: KeyCfg;
 };
+export type KeySmartCfg = {
+  name: string;
+  super: number[];
+  mt: number[];
+};
+export type LeyerKeysConfig = {
+  config: number;
+  level: number;
+  name: string;
+  def: {
+    tary: number[];
+  };
+  keys: KeyCfgMap;
+  smart: KeySmartCfg[];
+  disable: string[];
+};
