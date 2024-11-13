0b08ff8ec3d1f535d818fd957a89a9479cb57a1c
CorbinV <vcorbin.wynn@gmail.com>
2024-11-13 16:37:12 +0800

    feat: 新增按键信息及其控制组件

---
+++ src/components/custom/keyboard/components/key-control.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { computed, reactive, toRefs, watchEffect } from 'vue';
+import type { KeyInfo } from '@/api/modules/keyboard';
+import { useKeyboardStore } from '@/store/modules/keyboard';
+import { getKeyInfo, restoreKeyConfig, setKeyInfo } from '@/api/keyConfig';
+
+const keyboardStore = useKeyboardStore();
+
+type KeyControlProps = {
+  keyId?: string;
+};
+const props = withDefaults(defineProps<KeyControlProps>(), {
+  keyId: ''
+});
+const { kbCfg } = toRefs(keyboardStore);
+const keyInfo = reactive({
+  currentKey: {} as {
+    type: string;
+    label: string;
+    icon: string;
+  },
+  triggerPoint: 0,
+  rtTrigger: 0,
+  rtReset: 0,
+  params: []
+});
+const showIcon = computed(() => {
+  return keyInfo.currentKey?.type !== 'str';
+});
+function updateKeyInfo(data: KeyInfo) {
+  keyInfo.currentKey = kbCfg.value.keyMap[data!.type]?.code?.[data!.code] || {};
+  const [triggerPoint, _1, _2, rtTrigger, rtReset] = data.tary;
+  keyInfo.triggerPoint = triggerPoint;
+  keyInfo.rtTrigger = rtTrigger;
+  keyInfo.rtReset = rtReset;
+  if (data.mt.length) {
+    // feat: add new function to get config by key type and code
+    // keyInfo.params = ;
+  }
+}
+// optimize: add a notification to show the result
+watchEffect(async () => {
+  if (props.keyId === '') return;
+  const data = await getKeyInfo({
+    key: props.keyId
+  });
+  updateKeyInfo(data);
+});
+async function handleResetKey() {
+  const data = await restoreKeyConfig({
+    key: props.keyId
+  });
+  updateKeyInfo(data);
+  // optimize: add a notification to show the result
+}
+async function handleDisableKey() {
+  await setKeyInfo({
+    key: props.keyId,
+    enable: 0
+  });
+  // optimize: add a notification to show the result
+}
+
+// const
+</script>
+
+<template>
+  <div class="low-layer-bg flex flex-col gap-y-4 p-2">
+    <h1 class="text-center text-base">按键信息</h1>
+    <div class="text-c-primary flex flex-col">
+      <div class="flex flex-row justify-between border-b-1px border-#232327 py-3">
+        <span>默认{{ keyId || '/' }}</span>
+        <span>
+          当前
+          <i v-if="showIcon" class=""></i>
+          <span>
+            {{ keyInfo.currentKey?.label }}
+          </span>
+        </span>
+      </div>
+      <div class="flex flex-row justify-between border-b-1px border-#232327 py-3">
+        <span>触发点</span>
+        <span class="text-c-hl">{{ keyInfo.triggerPoint || '/' }}</span>
+      </div>
+      <div class="flex flex-row justify-between border-b-1px border-#232327 py-3">
+        <span>RT触发</span>
+        <span class="text-c-hl">{{ keyInfo.rtTrigger || '/' }}</span>
+      </div>
+      <div class="flex flex-row justify-between border-b-1px border-#232327 py-3">
+        <span>RT重置</span>
+        <span class="text-c-hl">{{ keyInfo.rtReset || '/' }}</span>
+      </div>
+      <!-- feat: finish the params -->
+      <!--
+      <div class="flex flex-row justify-between py-3">
+            <span>属性:</span>
+
+      <div class="flex flex-col"></div>
+              <span class="text-c-hl">{{ detail.tary[2] }}</span>
+      </div>
+      -->
+    </div>
+    <div>
+      <NButton type="info" ghost size="small" class="mr-2" @click="handleDisableKey">禁用按键</NButton>
+      <NButton type="info" ghost size="small" @click="handleResetKey">恢复默认</NButton>
+    </div>
+  </div>
+</template>
