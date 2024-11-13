c386b5257394878052bc94db1da6add195944186
ixzus <iandroid@foxmail.com>
2024-11-13 11:50:25 +0800

    feat(packetize): 长短包处理

---
+++ packages/packetize/src/index.ts
@@ -0,0 +1,77 @@
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+
+// 将字符串转为 Uint8Array
+export function jsonToArray(data: string): Uint8Array {
+  return encoder.encode(data);
+}
+
+// 将 Uint8Array 转为字符串
+export function arrayToJson(data: Uint8Array): string {
+  return decoder.decode(data);
+}
+
+// 将对象转为 Uint8Array
+export function objToArray(obj: any): Uint8Array {
+  return jsonToArray(JSON.stringify(obj));
+}
+
+let arr: number[] = [];
+function appendData(data: Uint8Array): void {
+  arr.push(...data);
+}
+
+// 解析数据包
+export function depacketize(data: Uint8Array): Uint8Array | undefined {
+  if (data.length < 4) return undefined;
+  switch (data[0]) {
+    case 0xa5:
+      return data.slice(2, -1);
+    case 0xa4:
+      if (data[2] > 0x80) {
+        arr = [];
+      }
+      appendData(data.slice(3, -1));
+      return data[2] === 1 ? new Uint8Array(arr) : undefined;
+    default:
+      return undefined;
+  }
+}
+
+// 将数据包拆分成多个小包
+export function packetize(data: Uint8Array, mtu: number = 48): Uint8Array[] {
+  const packages: Uint8Array[] = [];
+  if (data.length <= mtu) {
+    const header = 0xa5;
+    const packageLength = data.length + 3;
+    const packageBuffer = new Uint8Array(packageLength);
+    packageBuffer[0] = header;
+    packageBuffer[1] = packageLength;
+    packageBuffer.set(data, 2);
+    const checksum = packageBuffer.reduce((a, b) => a + b, 0) & 0xff;
+    packageBuffer[packageLength - 1] = checksum;
+    packages.push(packageBuffer);
+  } else {
+    const header = 0xa4;
+    const maxDataSize = mtu - 4;
+    const count = Math.ceil(data.length / maxDataSize);
+    for (let i = 0; i < count; i++) {
+      const start = i * maxDataSize;
+      const end = Math.min(start + maxDataSize, data.length);
+      const packageData = data.slice(start, end);
+      const packageLength = packageData.length + 4;
+      const packageBuffer = new Uint8Array(packageLength);
+      packageBuffer[0] = header;
+      packageBuffer[1] = packageLength;
+      packageBuffer[2] = count - i;
+      if (i === 0) {
+        packageBuffer[2] += 0x80;
+      }
+      packageBuffer.set(packageData, 3);
+      const checksum = packageBuffer.reduce((a, b) => a + b, 0) & 0xff;
+      packageBuffer[packageLength - 1] = checksum;
+      packages.push(packageBuffer);
+    }
+  }
+  return packages;
+}
