faa9f23a807f41ea85c6f64dd757e32b5f058782
CorbinV <vcorbin.wynn@gmail.com>
2024-11-15 17:17:20 +0800

    feat: 新增键值功能card 组件

---
+++ src/views/base-key/components/fnc-card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    code: number;
+    icon?: string;
+    label?: string;
+    type?: string;
+    description?: string;
+  }>(),
+  {
+    icon: 'placeholder',
+    description: '-'
+  }
+);
+</script>
+
+<template>
+  <div class="w-20 flex flex-col gap-y-6 hover:cursor-pointer" :data-code="code">
+    <div
+      class="higth-light-bg h-20 w-full inline-flex items-center justify-center border-1 border-#3C3933 rounded-md text-c-primary"
+    >
+      <p v-if="type === 'str'" class="text-center text-lg">
+        {{ label || '-' }}
+      </p>
+      <i v-else class="iconfont text-3xl" :class="`icon-${icon}`"></i>
+    </div>
+    <p class="text-center text-c-second">{{ description }}</p>
+  </div>
+</template>
