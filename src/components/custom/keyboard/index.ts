40e24021d386ec9b52b1f5385d964e0ece50210b
CorbinV <vcorbin.wynn@gmail.com>
2024-11-15 17:16:13 +0800

    feat: 添加键盘相关组件导出

---
+++ src/components/custom/keyboard/index.ts
@@ -0,0 +1,5 @@
+import DynamicKeyboard from './dynamic.vue';
+import StandardKeyboard from './standard.vue';
+import KeyboardContainer from './container.vue';
+export { DynamicKeyboard, StandardKeyboard, KeyboardContainer };
+export default KeyboardContainer;
