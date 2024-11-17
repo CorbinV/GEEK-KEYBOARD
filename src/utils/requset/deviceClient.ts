a856ed1d6d1457195db299dd8b11a07d6a143f1e
CorbinV <vcorbin.wynn@gmail.com>
2024-11-17 13:54:46 +0800

    optimisze: api 基础层改造，消息处理实体支持动态变更

--- src/utils/requset/deviceClient.ts
+++ src/utils/requset/deviceClient.ts
@@ -5,10 +5,10 @@ export type SendOps = {
 export type SendCfg = {
   waitResponse?: boolean;
 };
-export type MessageConfig = SendOps &
-  SendCfg & {
-    // optimize: other params
-  };
+// export type MessageConfig = SendOps &
+//   SendCfg & {
+//     // optimize: other params
+//   };
 export type RespOps<T = any> = {
   name: string;
   code: number;
@@ -27,29 +27,38 @@ function getAllMock() {
 const mockData = useMock ? getAllMock() : {};
 
 export class UsbTransfor {
-  request<T = any>(config: MessageConfig): Promise<RespOps<T>> {
+  /**
+   * @description: Transport dependencies.
+   *               In a production environment, an exception is thrown
+   *               if the device calls the data transfer function without instantiation.
+   */
+  private communicator: any;
+  constructor() {
+    this.communicator = undefined;
+  }
+  getCommunicator() {
+    if (!this.communicator) {
+      throw new Error('Please initialize the device first');
+    }
+    return this.communicator;
+  }
+  setCommunicator(communicator: any) {
+    this.communicator = communicator;
+  }
+  request<T = any>(ops: SendOps, _cfg?: SendCfg): Promise<RespOps<T>> {
     if (useMock) {
       return new Promise(resolve => {
-        const data = mockData[config.name];
+        const data = mockData[ops.name];
         resolve(data);
       });
     }
     // feat: use device to send data
-    return new Promise(res => {
-      res({
-        name: config.name,
-        code: 0,
-        data: {} as T
-      });
-    });
+    return this.getCommunicator().send(ops);
   }
   async send<T = any>(opstions: SendOps, cfg: SendCfg = { waitResponse: true }): Promise<T> {
     // optimize: transform options and config to request
     const sendOps = JSON.parse(JSON.stringify(opstions));
-    const { code, data } = await this.request<T>({
-      ...sendOps,
-      ...cfg
-    });
+    const { code, data } = await this.request<T>(sendOps, cfg);
     if (code !== 0) {
       throw new Error('error');
     }
