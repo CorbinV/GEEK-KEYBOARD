af8ec193accb067f86344e93b27571e344185d28
CorbinV <vcorbin.wynn@gmail.com>
2024-11-18 14:29:30 +0800

    feat: 新增通信管理层

---
+++ src/utils/requset/connectMannager.ts
@@ -0,0 +1,25 @@
+import { HIDProtocolController } from '@sa/device-transfer';
+import { UsbTransfor } from './deviceClient';
+export class ConnectionManager {
+  private static instance: ConnectionManager;
+  private deviceClient: UsbTransfor;
+  private hidController: HIDProtocolController;
+
+  constructor() {
+    this.deviceClient = new UsbTransfor();
+    this.hidController = new HIDProtocolController();
+  }
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new ConnectionManager();
+    }
+    return this.instance;
+  }
+  async connectDevice(deviceConfig: any) {
+    await this.hidController.connect(deviceConfig);
+    this.deviceClient.setCommunicator(this.hidController);
+  }
+  getDeviceClient() {
+    return this.deviceClient;
+  }
+}
