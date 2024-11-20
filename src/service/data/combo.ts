fc6c1023d770bfd7596d53912521cf550a9681c8
CorbinV <vcorbin.wynn@gmail.com>
2024-11-20 17:51:29 +0800

    feat(mock):  新增组件键相关mock数据

---
+++ src/service/data/combo.ts
@@ -0,0 +1,38 @@
+export const getShortcuts = {
+  name: 'getShortcuts',
+  code: 0,
+  data: {
+    shortcuts: [
+      {
+        type: 3,
+        code: 4
+      },
+      {
+        type: 3,
+        code: 5
+      }
+    ]
+  }
+};
+export const getShortcut = {
+  name: 'getShortcut',
+  code: 0,
+  data: {
+    type: 3,
+    code: 1,
+    keys: [
+      {
+        type: 0,
+        code: 4
+      },
+      {
+        type: 0,
+        code: 5
+      }
+    ]
+  }
+};
+export const addShortcut = {
+  name: 'addShortcut',
+  code: 0
+};
