f9c48241c9a5d06c0111b52a82c7ef9fac7047c6
CorbinV <vcorbin.wynn@gmail.com>
2024-11-21 14:01:37 +0800

    perf: 组合编辑组件逻辑优化

--- src/views/base-key/components/combo-edit.vue
+++ src/views/base-key/components/combo-edit.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import type { TabsProps } from 'naive-ui';
-import { reactive, ref, toRefs, watchEffect } from 'vue';
+import { nextTick, reactive, toRefs, watchEffect } from 'vue';
 import { useKeyboardStore } from '@/store/modules/keyboard';
 import { KeyTypeEnum } from '@/enum/keyType';
 import BaseKey from '@/components/custom/keyboard/components/base-key.vue';
 import { createComboGroup } from '@/api/combo';
 import { StandardKeyboard } from '@/components/custom/keyboard';
+import { useResttableRefFn } from '@/hooks/common/basicFnc';
 import ModuleTemplate from './module-template.vue';
-const emit = defineEmits(['update:visible']);
+const emit = defineEmits(['update:visible', 'create-group']);
 
 const keyboardStore = useKeyboardStore();
 const { kbCfg } = toRefs(keyboardStore);
@@ -15,45 +16,43 @@ const props = defineProps<{
   groupLength: number;
   visible: boolean;
 }>();
+
 type TabsPropsThemeOverrides = NonNullable<TabsProps['themeOverrides']>;
 const tabsThemeOverrides: TabsPropsThemeOverrides = {};
+
 function useDialogController() {
   const control = reactive({
-    visible: true,
-    content: []
+    visible: true
   });
 
-  const baseKeyList = ref([]);
   watchEffect(() => {
     control.visible = props.visible;
   });
   return {
     dialogControl: control,
-    baseKeyList,
     openDialog: () => {
       control.visible = true;
     },
     closeDialog: () => {
       control.visible = false;
       emit('update:visible', false);
-    },
-    resetContent: () => {
-      control.content = [];
     }
   };
 }
 const { dialogControl, closeDialog } = useDialogController();
-const selectedKeyInfo = reactive<any>({
+
+// event handler
+const [selectedKeyInfo, resetSelectedKeyInfo] = useResttableRefFn<any>(() => ({
   idx: 0,
   list: [{}]
-});
+}));
 function handleKeyClicked(e: MouseEvent) {
   const targetElement = (e.target as Element).closest('[data-idx]');
   if (targetElement && targetElement instanceof HTMLElement) {
     const idx = targetElement.dataset.idx;
-    selectedKeyInfo.idx = Number(idx);
-    if (!selectedKeyInfo.list[selectedKeyInfo.idx]) {
-      selectedKeyInfo.list[selectedKeyInfo.idx] = {
+    selectedKeyInfo.value.idx = Number(idx);
+    if (!selectedKeyInfo.value.list[selectedKeyInfo.value.idx]) {
+      selectedKeyInfo.value.list[selectedKeyInfo.value.idx] = {
         base: {},
         detail: []
       };
@@ -63,20 +62,26 @@ function handleKeyClicked(e: MouseEvent) {
 function handleFncClicked({ code, type }: { code: number; type: KeyTypeEnum }) {
   const codeMap = kbCfg.value.keyMap[type].code;
   const data = codeMap[`${code}`];
-  selectedKeyInfo.list[selectedKeyInfo.idx].base = { code, type };
-  selectedKeyInfo.list[selectedKeyInfo.idx].detail = data;
+  selectedKeyInfo.value.list[selectedKeyInfo.value.idx].base = { code, type };
+  selectedKeyInfo.value.list[selectedKeyInfo.value.idx].detail = data;
 }
 async function handleDialogComfirm() {
   const sendData = {
     type: KeyTypeEnum.Combo,
-    code: props.groupLength,
-    keys: selectedKeyInfo.list.map((item: any) => item.base)
+    code: props.groupLength + 1,
+    keys: selectedKeyInfo.value.list.map((item: any) => item.base)
   };
   try {
     await createComboGroup(sendData);
-
-    closeDialog();
+    emit('create-group', {
+      ...sendData,
+      detail: JSON.parse(JSON.stringify(selectedKeyInfo.value))
+    });
     window.$message!.success('创建成功');
+    nextTick(() => {
+      resetSelectedKeyInfo();
+      closeDialog();
+    });
   } catch (error) {
     window.$message!.error('创建失败');
     console.error(error);
