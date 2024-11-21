7a46b08c63b8f5157c9370d6947f6a32f9813f91
CorbinV <vcorbin.wynn@gmail.com>
2024-11-21 14:30:26 +0800

    style: 样式调整

--- src/components/custom/basic-group-add.vue
+++ src/components/custom/basic-group-add.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <div
-    class="w-360px inline-flex items-center justify-center border border-#3C8DF4 rounded-md border-dashed py-6 text-#3C8DF4 hover:cursor-pointer"
+    class="min-w-120px inline-flex items-center justify-center border border-#3C8DF4 rounded-md border-dashed py-6 text-#3C8DF4 hover:cursor-pointer"
   >
     <div class="flex flex-col justify-center gap-y-1">
       <i class="iconfont text-center text-2xl font-bold" :class="`icon-${icon}`"></i>
