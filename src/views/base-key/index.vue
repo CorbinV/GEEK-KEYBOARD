5aaddf99076fa53858d12c6b6bb8ab6cf2c04882
CorbinV <vcorbin.wynn@gmail.com>
2024-11-22 01:16:54 +0800

    feat: 挂载键值模块挂载 基础/系统/特殊 逻辑块

--- src/views/base-key/index.vue
+++ src/views/base-key/index.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { KeyboardContainer, StandardKeyboard } from '@/components/custom/keyboard/index';
+import { KeyTypeEnum } from '@/enum/keyType';
+import ModuleTemplate from './components/module-template.vue';
+</script>
 
 <template>
-  <div>base-key</div>
+  <div>
+    <KeyboardContainer>
+      <template #default="{ handleKeyEmit }">
+        <NTabs default-value="combo" size="large" justify-content="space-evenly" placement="bottom">
+          <NTabPane name="basic" tab="基础">
+            <StandardKeyboard @key-clicked="data => handleKeyEmit({ ...data, type: 0 })" />
+          </NTabPane>
+          <NTabPane name="system" tab="系统">
+            <ModuleTemplate :type="KeyTypeEnum.System" @key-clicked="handleKeyEmit" />
+          </NTabPane>
+          <NTabPane name="media" tab="媒体">
+            <ModuleTemplate :type="KeyTypeEnum.Media" @key-clicked="handleKeyEmit" />
+          </NTabPane>
+          <NTabPane name="special" tab="特殊">
+            <ModuleTemplate :type="KeyTypeEnum.Special" @key-clicked="handleKeyEmit" />
+          </NTabPane>
+        </NTabs>
+      </template>
+    </KeyboardContainer>
+  </div>
 </template>
 
 <style scoped></style>
