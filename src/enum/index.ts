79ffc9143c272c841c3a4be3eb926eab9834d285
ixzus <iandroid@foxmail.com>
2024-11-22 18:26:10 +0800

    fix: 枚举缺失，进入首页

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -4,5 +4,6 @@ export enum SetupStoreId {
   Auth = 'auth-store',
   Route = 'route-store',
   Tab = 'tab-store',
-  Keyboard = 'keyboard-store'
+  Keyboard = 'keyboard-store',
+  Common = 'common-store'
 }
