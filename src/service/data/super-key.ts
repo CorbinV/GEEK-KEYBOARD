5f446d16ff870adafe4a3cff7317d102685f04d0
CorbinV <vcorbin.wynn@gmail.com>
2024-11-25 18:34:26 +0800

    feat(mock): 新增超级键 oks 相关模拟数据

---
+++ src/service/data/super-key.ts
@@ -0,0 +1,69 @@
+// oks
+export const getOKSList = {
+  name: 'getOKSList',
+  code: 0,
+  data: {
+    oks: [
+      {
+        type: 7,
+        code: 1,
+        name: '快速急停AD',
+        keys: [
+          {
+            code: 4,
+            type: 0
+          },
+          {
+            code: 32,
+            type: 0
+          }
+        ]
+      },
+      {
+        type: 7,
+        code: 2,
+        name: '快速急停WS',
+        keys: [
+          {
+            code: 4,
+            type: 0
+          },
+          {
+            code: 32,
+            type: 0
+          }
+        ]
+      }
+    ]
+  }
+};
+export const setOKSName = {
+  name: 'setOKSName',
+  code: 0
+};
+export const delOKS = {
+  name: 'delOKS',
+  code: 0
+};
+export const getOKS = {
+  name: 'getOKS',
+  code: 0,
+  data: {
+    code: 1,
+    name: '快速急停AD',
+    keys: [
+      {
+        code: 4,
+        type: 0
+      },
+      {
+        code: 32,
+        type: 0
+      }
+    ]
+  }
+};
+export const setOKS = {
+  name: 'setOKS',
+  code: 0
+};
