6c73ce8b951c88293c9bbd61d5e72fb0fac45b24
CorbinV <vcorbin.wynn@gmail.com>
2024-11-26 16:43:05 +0800

    optimize: 键盘新增标准导出

--- src/components/custom/keyboard/index.ts
+++ src/components/custom/keyboard/index.ts
@@ -1,5 +1,6 @@
 import DynamicKeyboard from './dynamic.vue';
 import StandardKeyboard from './standard.vue';
 import KeyboardContainer from './container.vue';
-export { DynamicKeyboard, StandardKeyboard, KeyboardContainer };
+import BaseKeyboard from './components/keyboard.vue';
+export { DynamicKeyboard, StandardKeyboard, KeyboardContainer, BaseKeyboard };
 export default KeyboardContainer;
