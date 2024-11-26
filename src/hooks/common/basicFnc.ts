4a35b29569a11b7287ee32bbf7ec5488687e528c
CorbinV <vcorbin.wynn@gmail.com>
2024-11-26 16:29:54 +0800

    feat: 新增函数支持 reactive 类型数据重置

--- src/hooks/common/basicFnc.ts
+++ src/hooks/common/basicFnc.ts
@@ -1,9 +1,20 @@
-import type { Ref } from 'vue';
-import { ref } from 'vue';
+import type { Reactive, Ref } from 'vue';
+import { reactive, ref } from 'vue';
 export function useResttableRefFn<T>(value: () => T): [Ref<T>, () => void] {
   const data = ref(value()) as Ref<T>;
   const reset = () => {
     data.value = value();
   };
   return [data, reset];
+}
+export function useResttableReactiveFn<T extends object>(value: () => T): [Reactive<T>, () => void] {
+  const data = reactive(value()) as Reactive<T>;
+  const reset = () => {
+    Object.keys(data).forEach(key => {
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      delete (data as any)[key];
+    });
+    Object.assign(data, value());
+  };
+  return [data, reset] as const;
 }
