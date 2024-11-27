0b0085d8d4e7a98e9459a14f35196707b5fe9d98
CorbinV <vcorbin.wynn@gmail.com>
2024-11-27 09:47:33 +0800

    feat: 超级键页挂载

--- src/views/super-key/index.vue
+++ src/views/super-key/index.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { toRef } from 'vue';
+import { KeyboardContainer } from '@/components/custom/keyboard/index';
+import { useKeyboardStore } from '@/store/modules/keyboard';
+import Oks from './modules/oks.vue';
+const keyboardStore = useKeyboardStore();
+const allowMutipleSelect = toRef(keyboardStore, 'allowMutipleSelect');
+allowMutipleSelect.value = false;
+</script>
 
 <template>
-  <div>super-key</div>
+  <div>
+    <KeyboardContainer>
+      <template #keyboardBottom></template>
+      <template #default="{ handleKeyEmit }">
+        <!-- <div>点击测试按键</div> -->
+
+        <NTabs
+          default-value="oks"
+          size="large"
+          justify-content="space-evenly"
+          placement="bottom"
+          class="h-full"
+          pane-class="h-full"
+        >
+          <NTabPane name="oks" tab="OKS单键急停">
+            <Oks @key-clicked="handleKeyEmit" />
+          </NTabPane>
+        </NTabs>
+      </template>
+    </KeyboardContainer>
+  </div>
 </template>
 
 <style scoped></style>
