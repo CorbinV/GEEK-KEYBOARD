e8e450bb430b260565cc33f758c803b56a2ff72e
CorbinV <vcorbin.wynn@gmail.com>
2024-11-27 09:54:09 +0800

    optimize: 超级键 oks块弹出窗开启逻辑调整

--- src/views/super-key/modules/oks.vue
+++ src/views/super-key/modules/oks.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, toRef } from 'vue';
+import { onMounted, ref } from 'vue';
 import BasicGroupItem from '@/components/custom/basic-group-item.vue';
 import BasicGroupAdd from '@/components/custom/basic-group-add.vue';
 import { useKeyboardStore } from '@/store/modules/keyboard';
@@ -14,16 +14,16 @@ const MAC_GORUP_CNT = 8;
 const emit = defineEmits(['key-clicked']);
 const keyboardStore = useKeyboardStore();
 const { getKeyDetail } = keyboardStore;
-const selectedKeys = toRef(keyboardStore, 'selectedKeys');
+// const selectedKeys = toRef(keyboardStore, 'selectedKeys');
 function handleAddClicked() {
   if (oksGroupList.value.length >= MAC_GORUP_CNT) {
     window.$message!.warning(`最多只能添加${MAC_GORUP_CNT}个组合键`);
     return;
   }
-  if (Object.keys(selectedKeys.value).length === 0) {
-    window.$message!.info('请选择按键');
-    return;
-  }
+  // if (Object.keys(selectedKeys.value).length === 0) {
+  //   window.$message!.info('请选择按键');
+  //   return;
+  // }
   editVisible.value = true;
 }
 onMounted(async () => {
@@ -79,6 +79,10 @@ async function handleGroupItemEdit(items: any, idx: number) {
   // feat: open edit modal(dialog), and transform data
   console.log('handleGroupItemEdit', items, idx);
 }
+async function handleGroupItemRename(items: any, idx: number) {
+  // feat: rename group name
+  console.log('handleGroupItemRename', items, idx);
+}
 function generateGroupCode() {
   if (oksGroupList.value.length === 0) return 1;
   const usedCodes = new Set(oksGroupList.value.map((group: { code: number }) => group.code));
@@ -109,7 +113,7 @@ function generateGroupCode() {
             :enable-edit="false"
             @group-item-delete="handleGroupItemDelete"
             @group-item-edit="handleGroupItemEdit"
-            @group-item-rename="handleGroupItemEdit"
+            @group-item-rename="handleGroupItemRename"
           />
         </template>
       </BasicGroupItem>
