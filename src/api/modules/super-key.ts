7d96a6832125fec75f0ca1c7681dc2f5de5bdeaa
ixzus <iandroid@foxmail.com>
2024-11-27 20:28:00 +0800

    feat: 新增超级键socd、rs接口api

--- src/api/modules/super-key.ts
+++ src/api/modules/super-key.ts
@@ -12,3 +12,31 @@ export type OksBase = Omit<OksItem, 'type'>;
 export type OksList = {
   oks: OksItem[];
 };
+
+// socd
+export type SOCDItem = BaseKey & {
+  name: string;
+  keys: BaseKey[];
+};
+export type ResetSOCDName = {
+  code: number;
+  name: string;
+};
+export type SOCDBase = Omit<SOCDItem, 'type'>;
+export type SOCDList = {
+  socd: SOCDItem[];
+};
+
+// rs
+export type RSItem = BaseKey & {
+  name: string;
+  keys: BaseKey[];
+};
+export type ResetRSName = {
+  code: number;
+  name: string;
+};
+export type RSBase = Omit<RSItem, 'type'>;
+export type RSList = {
+  rs: RSItem[];
+};
--- src/api/super-key.ts
+++ src/api/super-key.ts
@@ -1,5 +1,15 @@
 import requestClient from '@/utils/requset/deviceClient';
-import type { OksBase, OksList, ResetOksName } from './modules/super-key';
+import type {
+  OksBase,
+  OksList,
+  RSBase,
+  RSList,
+  ResetOksName,
+  ResetRSName,
+  ResetSOCDName,
+  SOCDBase,
+  SOCDList
+} from './modules/super-key';
 import type { BaseKey } from './modules/combo';
 export function getOksList() {
   return requestClient.send<OksList>({
@@ -27,6 +37,68 @@ export function getTargetOks(data: BaseKey) {
 export function addOks(data: OksBase) {
   return requestClient.send<never>({
     name: 'setOKS',
+    data
+  });
+}
+
+// socd
+export function getSOCDList() {
+  return requestClient.send<SOCDList>({
+    name: 'getSOCDList'
+  });
+}
+export function resetSOCDName(data: ResetSOCDName) {
+  return requestClient.send<never>({
+    name: 'setSOCDName',
+    data
+  });
+}
+export function deleteSOCDByCode(data: { code: number }) {
+  return requestClient.send<never>({
+    name: 'delSOCD',
+    data
+  });
+}
+export function getTargetSOCD(data: BaseKey) {
+  return requestClient.send<never>({
+    name: 'getSOCD',
+    data
+  });
+}
+export function addSOCD(data: SOCDBase) {
+  return requestClient.send<never>({
+    name: 'setSOCD',
+    data
+  });
+}
+
+// rs
+export function getRSList() {
+  return requestClient.send<RSList>({
+    name: 'getRSList'
+  });
+}
+export function resetRSName(data: ResetRSName) {
+  return requestClient.send<never>({
+    name: 'setRSName',
+    data
+  });
+}
+export function deleteRSByCode(data: { code: number }) {
+  return requestClient.send<never>({
+    name: 'delRS',
+    data
+  });
+}
+export function getTargetRS(data: BaseKey) {
+  return requestClient.send<never>({
+    name: 'getRS',
+    data
+  });
+}
+export function addRS(data: RSBase) {
+  return requestClient.send<never>({
+    name: 'setRS',
     data
   });
 }
