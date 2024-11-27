33c77950efa9b9fd5ee8cafc66008d654cc96c3f
ixzus <iandroid@foxmail.com>
2024-11-27 20:29:24 +0800

    feat(mock): 新增socd、rs模拟数据

--- src/service/data/super-key.ts
+++ src/service/data/super-key.ts
@@ -10,10 +10,12 @@ export const getOKSList = {
         name: '快速急停AD',
         keys: [
           {
+            key: 'A',
             code: 4,
             type: 0
           },
           {
+            key: '#3',
             code: 32,
             type: 0
           }
@@ -25,11 +27,13 @@ export const getOKSList = {
         name: '快速急停WS',
         keys: [
           {
-            code: 4,
+            key: 'B',
+            code: 5,
             type: 0
           },
           {
-            code: 32,
+            key: '$4',
+            code: 33,
             type: 0
           }
         ]
@@ -39,7 +43,10 @@ export const getOKSList = {
 };
 export const setOKSName = {
   name: 'setOKSName',
-  code: 0
+  data: {
+    code: 1,
+    name: '快速急停AD'
+  }
 };
 export const delOKS = {
   name: 'delOKS',
@@ -53,10 +60,12 @@ export const getOKS = {
     name: '快速急停AD',
     keys: [
       {
+        key: 'A',
         code: 4,
         type: 0
       },
       {
+        key: '#3',
         code: 32,
         type: 0
       }
@@ -65,5 +74,218 @@ export const getOKS = {
 };
 export const setOKS = {
   name: 'setOKS',
+  data: {
+    code: 1,
+    name: '快速急停AD',
+    keys: [
+      {
+        key: 'A',
+        code: 4,
+        type: 0
+      },
+      {
+        key: '#3',
+        code: 32,
+        type: 0
+      }
+    ]
+  }
+};
+
+// socd
+export const getSOCDList = {
+  name: 'getSOCDList',
+  code: 0,
+  data: {
+    socd: [
+      {
+        type: 7,
+        code: 1,
+        trigger: 1,
+        name: 'SOCD1',
+        keys: [
+          {
+            key: 'B',
+            code: 5,
+            type: 0
+          },
+          {
+            key: 'C',
+            code: 6,
+            type: 0
+          }
+        ]
+      },
+      {
+        type: 7,
+        code: 2,
+        trigger: 2,
+        name: 'SOCD2',
+        keys: [
+          {
+            key: 'D',
+            code: 7,
+            type: 0
+          },
+          {
+            key: 'E',
+            code: 8,
+            type: 0
+          }
+        ]
+      }
+    ]
+  }
+};
+export const setSOCDName = {
+  name: 'setSOCDName',
+  data: {
+    code: 1,
+    name: 'SOCD1'
+  }
+};
+export const delSOCD = {
+  name: 'delSOCD',
   code: 0
+};
+export const getSOCD = {
+  name: 'getSOCD',
+  code: 0,
+  data: {
+    code: 1,
+    name: 'SOCD1',
+    trigger: 1,
+    keys: [
+      {
+        key: 'B',
+        code: 5,
+        type: 0
+      },
+      {
+        key: 'C',
+        code: 6,
+        type: 0
+      }
+    ]
+  }
+};
+export const setSOCD = {
+  name: 'setSOCD',
+  data: {
+    type: 8,
+    code: 1,
+    trigger: 1,
+    name: 'SOCD1',
+    keys: [
+      {
+        key: 'B',
+        code: 5,
+        type: 0
+      },
+      {
+        key: 'C',
+        code: 6,
+        type: 0
+      }
+    ]
+  }
+};
+
+// rs
+export const getRSList = {
+  name: 'getRSList',
+  code: 0,
+  data: {
+    rs: [
+      {
+        type: 7,
+        code: 1,
+        trigger: 1,
+        name: 'RS1',
+        keys: [
+          {
+            key: 'H',
+            code: 11,
+            type: 0
+          },
+          {
+            key: 'I',
+            code: 12,
+            type: 0
+          }
+        ]
+      },
+      {
+        type: 7,
+        code: 2,
+        trigger: 2,
+        name: 'RS2',
+        keys: [
+          {
+            key: 'J',
+            code: 13,
+            type: 0
+          },
+          {
+            key: 'K',
+            code: 14,
+            type: 0
+          }
+        ]
+      }
+    ]
+  }
+};
+export const setRSName = {
+  name: 'setRSName',
+  data: {
+    code: 1,
+    name: 'RS1'
+  }
+};
+export const delRS = {
+  name: 'delRS',
+  code: 0
+};
+export const getRS = {
+  name: 'getRS',
+  code: 0,
+  data: {
+    code: 1,
+    name: 'RS1',
+    trigger: 1,
+    keys: [
+      {
+        key: 'H',
+        code: 11,
+        type: 0
+      },
+      {
+        key: 'I',
+        code: 12,
+        type: 0
+      }
+    ]
+  }
+};
+export const setRS = {
+  name: 'setRS',
+  data: {
+    type: 8,
+    code: 1,
+    trigger: 1,
+    name: 'RS1',
+    keys: [
+      {
+        key: 'H',
+        code: 11,
+        type: 0
+      },
+      {
+        key: 'I',
+        code: 12,
+        type: 0
+      }
+    ]
+  }
 };
