c2e6cdc001e51df0b2ee8d258668f00dbd640fb5
ixzus <iandroid@foxmail.com>
2024-11-27 20:43:49 +0800

    feat: 新增超级键rs页面

---
+++ src/views/super-key/modules/rs.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import BasicGroupItem from '@/components/custom/basic-group-item.vue';
+import BasicGroupAdd from '@/components/custom/basic-group-add.vue';
+import { useKeyboardStore } from '@/store/modules/keyboard';
+import { KeyTypeEnum } from '@/enum/keyType';
+import { addRS, deleteRSByCode, getRSList } from '@/api/super-key';
+import EditTemplate from '../components/edit-template.vue';
+import GroupMenu from '../components/group-menu.vue';
+const rsGroupList = ref<any>([]);
+const editVisible = ref(false);
+const modalTitle = ref('灵动触发按键');
+const MAC_GORUP_CNT = 8;
+const emit = defineEmits(['key-clicked']);
+const keyboardStore = useKeyboardStore();
+const { getKeyDetail } = keyboardStore;
+// const selectedKeys = toRef(keyboardStore, 'selectedKeys');
+function handleAddClicked() {
+  if (rsGroupList.value.length >= MAC_GORUP_CNT) {
+    window.$message!.warning(`最多只能添加${MAC_GORUP_CNT}个组合键`);
+    return;
+  }
+  // if (Object.keys(selectedKeys.value).length === 0) {
+  //   window.$message!.info('请选择按键');
+  //   return;
+  // }
+  editVisible.value = true;
+}
+onMounted(async () => {
+  const { rs } = await getRSList();
+  rsGroupList.value = rs.map(item => {
+    const { code, type, name } = item;
+    return {
+      base: { code, type, name },
+      keyList: item.keys.map(keyBase => {
+        const res = getKeyDetail({ code: keyBase.code, type: keyBase.type });
+        return res;
+      })
+    };
+  });
+});
+async function handleGroupCreated({ code, keys, name, listDetail }: any) {
+  try {
+    await addRS({ code, keys, name });
+    rsGroupList.value.push({
+      base: {
+        code,
+        name
+      },
+      keyList: listDetail.map((item: any) => {
+        return item.detail;
+      })
+    });
+    window.$message!.success('添加成功');
+  } catch (e) {
+    window.$message!.error('添加失败，请更新最新固件后重试');
+    console.error(e);
+  }
+}
+
+function handleGroupItemClicked({ base }: { base: { code: number; type: KeyTypeEnum.Combo } }) {
+  const { code, type } = base;
+  emit('key-clicked', {
+    code,
+    type
+  });
+}
+async function handleGroupItemDelete(item: { code: number }, idx: number) {
+  try {
+    await deleteRSByCode({ code: item.code });
+    rsGroupList.value.splice(idx, 1);
+    window.$message!.success('删除成功');
+  } catch (error) {
+    window.$message!.error('删除失败，请更新最新固件后重试');
+    console.error(error);
+  }
+}
+async function handleGroupItemEdit(items: any, idx: number) {
+  // feat: open edit modal(dialog), and transform data
+  console.log('handleGroupItemEdit', items, idx);
+}
+async function handleGroupItemRename(items: any, idx: number) {
+  // feat: rename group name
+  console.log('handleGroupItemRename', items, idx);
+}
+function generateGroupCode() {
+  if (rsGroupList.value.length === 0) return 1;
+  const usedCodes = new Set(rsGroupList.value.map((group: { code: number }) => group.code));
+  let newCode = 1;
+  while (usedCodes.has(newCode)) {
+    newCode++;
+  }
+  return newCode;
+}
+</script>
+
+<template>
+  <div>
+    <div class="grid grid-cols-4 mx-auto my-0 gap-x-4 gap-y-8 p-4">
+      <BasicGroupAdd icon="add" desc="添加灵动触发按键" @click="handleAddClicked" />
+      <BasicGroupItem
+        v-for="(item, idx) in rsGroupList"
+        :key="item.code"
+        :base="item.base"
+        :key-list="item.keyList"
+        code-preffix="R"
+        @click="handleGroupItemClicked(item)"
+      >
+        <template #menu>
+          <GroupMenu
+            :group-item="item"
+            :idx="idx"
+            :enable-edit="false"
+            @group-item-delete="handleGroupItemDelete"
+            @group-item-edit="handleGroupItemEdit"
+            @group-item-rename="handleGroupItemRename"
+          />
+        </template>
+      </BasicGroupItem>
+    </div>
+    <EditTemplate
+      v-model:visible="editVisible"
+      v-model:title="modalTitle"
+      :code-type="KeyTypeEnum.RS"
+      :fnc-generate-code="generateGroupCode"
+      second-title="监控两个按键，当两个按键同事按下时，触发按压更深的按键"
+      desc="*例：设置AB键为RS键，当A键按压比D键更深时，A键触发，抬起A键后，D键将恢复触发"
+      @create-group="handleGroupCreated"
+    ></EditTemplate>
+  </div>
+</template>
