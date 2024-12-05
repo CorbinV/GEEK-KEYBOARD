af5dcc4b0a9ced7e86d4a107a1e2f768de23af46
ixzus <iandroid@foxmail.com>
2024-12-05 15:28:10 +0800

    feat: 超级键tab样式

--- src/views/super-key/index.vue
+++ src/views/super-key/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { onUnmounted, toRef } from 'vue';
+import { onUnmounted, ref, toRef } from 'vue';
 import { KeyboardContainer } from '@/components/custom/keyboard/index';
 import { useKeyboardStore } from '@/store/modules/keyboard';
+import { KeyTypeEnum } from '@/enum/keyType';
 import Oks from './modules/oks.vue';
 import Socd from './modules/socd.vue';
 import MT from './modules/mt.vue';
@@ -10,30 +11,37 @@ import Rs from './modules/rs.vue';
 const keyboardStore = useKeyboardStore();
 const allowMutipleSelect = toRef(keyboardStore, 'allowMutipleSelect');
 allowMutipleSelect.value = false;
+
+const tabName = ref(KeyTypeEnum.OKS);
+function handleKeyEventTabs(value: string | number) {
+  tabName.value = Number(value);
+}
+
 onUnmounted(() => keyboardStore.resetCurrentSuperKeyType());
+
 const paneList = [
   {
-    name: 'oks',
+    name: KeyTypeEnum.OKS,
     label: 'OKS单键急停',
     component: Oks
   },
   {
-    name: 'socd',
+    name: KeyTypeEnum.SOCD,
     label: 'SOCD',
     component: Socd
   },
   {
-    name: 'mt',
+    name: KeyTypeEnum.MT,
     label: 'MT单击/按住',
     component: MT
   },
   {
-    name: 'tgl',
+    name: KeyTypeEnum.TGL,
     label: 'TGL切换开关',
     component: TGL
   },
   {
-    name: 'rs',
+    name: KeyTypeEnum.RS,
     label: 'RS',
     component: Rs
   }
@@ -47,11 +55,28 @@ const paneList = [
       <template #default="{ handleKeyEmit }">
         <!-- <div>点击测试按键</div> -->
 
-        <NTabs size="large" justify-content="space-evenly" placement="bottom" class="h-full" pane-class="h-full">
+        <!--
+ <NTabs size="large" justify-content="space-evenly" placement="bottom" class="h-full" pane-class="h-full">
           <NTabPane v-for="pane in paneList" :key="pane.name" :name="pane.name" :tab="pane.label">
             <component :is="pane.component" @key-clicked="handleKeyEmit" />
           </NTabPane>
         </NTabs>
+-->
+
+        <div class="h-full flex flex-col">
+          <div class="flex-1">
+            <component :is="paneList.find(item => item.name === tabName)?.component" @key-clicked="handleKeyEmit" />
+          </div>
+
+          <NTabs v-model:value="tabName" type="segment" animated @update:value="handleKeyEventTabs">
+            <NTab :name="KeyTypeEnum.DKS" tab="DKS动态键程" />
+            <NTab :name="KeyTypeEnum.OKS" tab="OKS单键急停" />
+            <NTab :name="KeyTypeEnum.SOCD" tab="SOCD" />
+            <NTab :name="KeyTypeEnum.MT" tab="MT单击/按住" />
+            <NTab :name="KeyTypeEnum.TGL" tab="TGL切换开关" />
+            <NTab :name="KeyTypeEnum.RS" tab="RS灵动触发" />
+          </NTabs>
+        </div>
       </template>
     </KeyboardContainer>
   </div>
