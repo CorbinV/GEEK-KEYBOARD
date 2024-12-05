505f3b1614f67ba987bca3dfde62dc599c0e26b9
ixzus <iandroid@foxmail.com>
2024-12-05 15:26:31 +0800

    feat: 键值tab样式

--- src/views/base-key/index.vue
+++ src/views/base-key/index.vue
@@ -1,35 +1,55 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { KeyboardContainer, StandardKeyboard } from '@/components/custom/keyboard/index';
 import { KeyTypeEnum } from '@/enum/keyType';
 import Macro from '../marco/components/Macro.vue';
 import ModuleTemplate from './components/module-template.vue';
 import Combo from './module/combo.vue';
+
+const tabName = ref(KeyTypeEnum.Normal);
+function handleKeyEventTabs(value: string | number) {
+  tabName.value = Number(value);
+}
 </script>
 
 <template>
   <div>
     <KeyboardContainer>
       <template #default="{ handleKeyEmit }">
-        <NTabs default-value="basic" size="large" justify-content="space-evenly" placement="bottom">
-          <NTabPane name="basic" tab="基础">
-            <StandardKeyboard @key-clicked="data => handleKeyEmit({ ...data, type: 0 })" />
-          </NTabPane>
-          <NTabPane name="system" tab="系统">
-            <ModuleTemplate :type="KeyTypeEnum.System" @key-clicked="handleKeyEmit" />
-          </NTabPane>
-          <NTabPane name="media" tab="媒体">
-            <ModuleTemplate :type="KeyTypeEnum.Media" @key-clicked="handleKeyEmit" />
-          </NTabPane>
-          <NTabPane name="combo" tab="组合">
-            <Combo @key-clicked="handleKeyEmit" />
-          </NTabPane>
-          <NTabPane name="special" tab="特殊">
-            <ModuleTemplate :type="KeyTypeEnum.Special" @key-clicked="handleKeyEmit" />
-          </NTabPane>
-          <NTabPane name="macro" tab="宏按键">
-            <Macro :edit="false" @key-clicked="handleKeyEmit" />
-          </NTabPane>
-        </NTabs>
+        <div class="h-full flex flex-col">
+          <div class="flex-1">
+            <StandardKeyboard
+              v-if="tabName === KeyTypeEnum.Normal"
+              @key-clicked="data => handleKeyEmit({ ...data, type: 0 })"
+            />
+            <ModuleTemplate
+              v-if="tabName === KeyTypeEnum.System"
+              :type="KeyTypeEnum.System"
+              @key-clicked="handleKeyEmit"
+            />
+            <ModuleTemplate
+              v-if="tabName === KeyTypeEnum.Media"
+              :type="KeyTypeEnum.Media"
+              @key-clicked="handleKeyEmit"
+            />
+            <Combo v-if="tabName === KeyTypeEnum.Combo" @key-clicked="handleKeyEmit" />
+            <ModuleTemplate
+              v-if="tabName === KeyTypeEnum.Special"
+              :type="KeyTypeEnum.Special"
+              @key-clicked="handleKeyEmit"
+            />
+            <Macro v-if="tabName === KeyTypeEnum.Marco" :edit="false" @key-clicked="handleKeyEmit" />
+          </div>
+
+          <NTabs v-model:value="tabName" type="segment" animated @update:value="handleKeyEventTabs">
+            <NTab :name="KeyTypeEnum.Normal" tab="基础" />
+            <NTab :name="KeyTypeEnum.System" tab="系统" />
+            <NTab :name="KeyTypeEnum.Media" tab="媒体" />
+            <NTab :name="KeyTypeEnum.Combo" tab="组合" />
+            <NTab :name="KeyTypeEnum.Special" tab="特殊" />
+            <NTab :name="KeyTypeEnum.Marco" tab="宏按键" />
+          </NTabs>
+        </div>
       </template>
     </KeyboardContainer>
   </div>
