b1c9c187673166af56bff2e2d65fa786bdb27638
ixzus <iandroid@foxmail.com>
2024-12-06 19:52:49 +0800

    feat: 连接页面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,54 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useAppStore } from '@/store/modules/app';
+import { toRefs, watchEffect } from 'vue';
+import { useDeviceStore } from '@/store/modules/device';
+import { router } from '@/router';
+import { useKeyboardStore } from '@/store/modules/keyboard';
+const deviceStore = useDeviceStore();
+const { isConnected } = toRefs(deviceStore);
 
-const appStore = useAppStore();
+const keyboardStore = useKeyboardStore();
+const { kbInfo } = toRefs(keyboardStore);
 
-const gap = computed(() => (appStore.isMobile ? 0 : 16));
+watchEffect(() => {
+  if (kbInfo.value.mounted) {
+    router.push('base-key');
+  }
+});
+async function handleConnectBtnClicked() {
+  console.log('handleConnectBtnClicked');
+  try {
+    await deviceStore.connect({
+      vendorId: 0x4353,
+      productId: 0x9108,
+      usagePage: 65408
+    });
+    console.log('Connected');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 </script>
 
 <template>
-  <NSpace vertical :size="16">
-    {{ gap }}
+  <div>
+    <div>
+      {{ isConnected }}
+      <NButton @click="handleConnectBtnClicked">connect</NButton>
+    </div>
+    <!-- <Dynamic class="h-2/5"/> -->
+    <!-- <KeyboardContainer /> -->
+    <!-- <StaticKeyboard /> -->
+    <!-- <Keyboard class="h-1/2 w-full" /> -->
+    <!--
+ <div>
+      <LayerControl :layer="cL" :layer-list="x"></LayerControl>
+    </div>
+-->
+  </div>
+  <!--
+ <NSpace vertical :size="16" class="relative">
+    <Keyboard />
   </NSpace>
-</template>
+--></template>
 
 <style scoped></style>
