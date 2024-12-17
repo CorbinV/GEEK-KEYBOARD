1150d27e7994f49b041d31afca698661208f04db
CorbinV <vcorbin.wynn@gmail.com>
2024-12-17 18:15:46 +0800

    feat: 新增 mitt 事件，部分代码重构

--- src/utils/eventBus.ts
+++ src/utils/eventBus.ts
@@ -1,9 +1,19 @@
 import mitt from 'mitt';
-
-type Events = {
-  resetKey: string;
+export enum EventNameEnum {
+  resetKey = 'resetKey',
+  selecteAll = 1,
+  reverseSelect,
+  selecteClear,
+  rtFncReset
+}
+export type MittEvents = {
+  [EventNameEnum.resetKey]: string;
+  [EventNameEnum.selecteAll]: null;
+  [EventNameEnum.reverseSelect]: null;
+  [EventNameEnum.selecteClear]: null;
+  [EventNameEnum.rtFncReset]: null;
 };
 
-const emitter = mitt<Events>();
+const emitter = mitt<MittEvents>();
 
 export default emitter;
