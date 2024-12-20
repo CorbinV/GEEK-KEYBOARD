9ccd77758530a11229c3ce7d78c033caaf1dcc4d
ixzus <iandroid@foxmail.com>
2024-12-20 16:18:39 +0800

    feat(mock): 在线升级模拟数据

---
+++ src/service/data/ota.ts
@@ -0,0 +1,14 @@
+export const OTAStart = {
+  name: 'OTAStart',
+  code: 0
+};
+
+export const OTAPacket = {
+  name: 'OTAPacket',
+  code: 0
+};
+
+export const OTAEnd = {
+  name: 'OTAEnd',
+  code: 0
+};
