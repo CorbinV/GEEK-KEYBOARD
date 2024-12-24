f1db13e7e7c5a38c64c4a17a17a0e4430cfb7729
ixzus <iandroid@foxmail.com>
2024-12-24 11:54:12 +0800

    feat: socd添加按键控制

--- src/views/super-key/modules/socd.vue
+++ src/views/super-key/modules/socd.vue
@@ -167,7 +167,7 @@ async function handleReNameSave(data: { name: string }) {
 <template>
   <div>
     <div class="grid grid-cols-4 mx-auto my-0 gap-x-4 gap-y-8 p-4">
-      <BasicGroupAdd icon="add" desc="添加SOCD按键" @click="handleAddClicked" />
+      <BasicGroupAdd v-if="socdGroupList.length < 8" icon="add" desc="添加SOCD按键" @click="handleAddClicked" />
       <BasicGroupItem
         v-for="(item, idx) in socdGroupList"
         :key="item.code"
