772ba9181a75ca3c3ef69b4a32a0dfb9e0ffd2c3
ixzus <iandroid@foxmail.com>
2024-12-28 16:41:04 +0800

    style: 调整宏列表样式,风格统一

--- src/views/marco/components/MacroList.vue
+++ src/views/marco/components/MacroList.vue
@@ -32,34 +32,41 @@ function handleMacrosMenu(key: string | number, item: Macro) {
 
 <template>
   <!-- list -->
-  <div class="grid grid-cols-4 gap-4 p-4">
+  <div class="grid grid-cols-4 mx-auto my-0 gap-x-4 gap-y-8 p-4">
     <!-- add -->
-    <div
+    <!--
+ <div
       v-if="canAddMacro"
-      class="h-25 flex flex-col items-center justify-center gap-2.5 border border border-[#3c8df4] rounded-lg border-dashed text-base text-[#3C8DF4] font-normal"
+      class="min-w-120px inline-flex items-center justify-center border border-#3C8DF4 rounded-md border-dashed py-6 text-#3C8DF4 hover:cursor-pointer"
       @click="handleNewMacro"
     >
-      <i class="iconfont icon-add" style="color: #3c8df4"></i>
-      <span class="text-[#3C8DF4]">{{ $t('macro.addMacroKey') }}</span>
+      <div class="flex flex-col justify-center gap-y-1">
+        <i class="iconfont icon-add text-center text-2xl font-bold"></i>
+        <span class="text-base">{{ $t('macro.addMacroKey') }}</span>
+      </div>
     </div>
+-->
+    <BasicGroupAdd v-if="canAddMacro" icon="add" :desc="$t('businessCommon.addSwtich')" @click="handleNewMacro" />
 
     <!-- item -->
-    <div v-for="item in macroList" :key="item.code" class="h-25 flex flex-col">
-      <div class="flex basis-2/3 items-center justify-center rounded-t-lg bg-[#131313]" @click="handleListItem(item)">
+    <div v-for="item in macroList" :key="item.code" class="flex flex-col">
+      <div class="h-20 flex items-center justify-center rounded-t-md higth-light-bg" @click="handleListItem(item)">
         <span class="text-sm text-[#999999] font-medium">{{ item.name }}</span>
       </div>
-      <div class="flex basis-1/3 items-center justify-between rounded-b-lg bg-[#222227] px-4">
-        <span class="text-sm text-[#999999] font-medium">M{{ item.code + 1 }}</span>
-        <NDropdown
-          v-if="props.edit"
-          trigger="hover"
-          :options="MacroType.MacrosOps"
-          @select="key => handleMacrosMenu(key, item)"
-        >
-          <div class="size-5 flex items-center justify-center rounded bg-[#1E1E22]">
-            <SvgIcon icon="tabler:dots" />
-          </div>
-        </NDropdown>
+      <div class="flex flex-row items-center justify-between rounded-b-md base-light-bg px-5 py-1">
+        <span class="text-c-primary">M{{ item.code + 1 }}</span>
+        <div class="rounded-md higth-light-bg px-3 hover:cursor-pointer">
+          <NDropdown
+            v-if="props.edit"
+            trigger="hover"
+            :options="MacroType.MacrosOps"
+            @select="key => handleMacrosMenu(key, item)"
+          >
+            <div class="py-1 text-c-primary">
+              <SvgIcon icon="tabler:dots" />
+            </div>
+          </NDropdown>
+        </div>
       </div>
     </div>
   </div>
