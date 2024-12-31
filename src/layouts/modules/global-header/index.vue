2e9eb936274a1b7cb87ed39f37738e43d6c10ec6
CorbinV <vcorbin.wynn@gmail.com>
2024-12-31 17:59:34 +0800

    feat: 仅在设备连接后挂载配置选择组件

--- src/layouts/modules/global-header/index.vue
+++ src/layouts/modules/global-header/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, defineAsyncComponent, onMounted, ref, watchEffect } from 'vue';
+import { useRoute } from 'vue-router';
 import { useAppStore } from '~/src/store/modules/app';
 import { useThemeStore } from '~/src/store/modules/theme';
 import { useRouteStore } from '~/src/store/modules/route';
@@ -10,7 +11,34 @@ import { useMixMenuContext } from '../../context';
 import ThemeButton from './components/theme-button.vue';
 // import UserAvatar from './components/user-avatar.vue';
 
+const AsyncConfigSelect = defineAsyncComponent({
+  loader: () => import('./components/config-select.vue'),
+  delay: 200,
+  // eslint-disable-next-line max-params
+  onError(error, retry, fail, attempts) {
+    if (error.message.includes('getActivePinia')) {
       // 如果是 Pinia 错误，可以尝试重试
+      if (attempts <= 3) {
+        retry();
+      } else {
+        fail();
+      }
+    } else {
+      fail();
+    }
+  }
+});
+const showCfg = ref(false);
+onMounted(() => {
+  const route = useRoute();
+  watchEffect(() => {
+    if (route.name === 'connect') {
+      showCfg.value = false;
+    } else {
+      showCfg.value = true;
+    }
+  });
+});
 defineOptions({
   name: 'GlobalHeader'
 });
@@ -47,13 +75,14 @@ const headerMenus = computed(() => {
 </script>
 
 <template>
-  <DarkModeContainer class="h-full flex-y-center shadow-header" style="background-color: #222226">
+  <DarkModeContainer class="h-full flex-y-center px-6 shadow-header" style="background-color: #222226">
     <GlobalLogo
       v-if="showLogo"
       :show-title="showTitle"
       class="h-full"
       :style="{ width: themeStore.sider.width + 'px' }"
     />
+    <AsyncConfigSelect v-if="showCfg" />
     <HorizontalMenu v-if="showMenu" mode="horizontal" :menus="headerMenus" class="win-drag px-12px" />
     <div v-else class="win-drag h-full flex-y-center flex-1-hidden">
       <MenuToggler
