dc50741c5d1fa2dcda3109a81af4370ddf0b0206
CorbinV <vcorbin.wynn@gmail.com>
2024-12-31 17:13:57 +0800

    feat: 新增配置选择组件

---
+++ src/layouts/modules/global-header/components/config-select.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed, toRef } from 'vue';
+import { useKeyboardStore } from '@/store/modules/keyboard';
+
+const keyboardStore = useKeyboardStore();
+const keyLayerInfo = toRef(keyboardStore, 'keyLayerInfo');
+
+const options = computed(() => {
+  const cnt = keyLayerInfo.value.configCount;
+  return Array.from({ length: cnt }).map((_, idx) => {
+    return {
+      label: `配置 ${idx + 1}`,
+      value: idx
+    };
+  });
+});
+</script>
+
+<template>
+  <NSelect v-model:value="keyLayerInfo.layerIndex" :options="options" class="w-24" />
+</template>
