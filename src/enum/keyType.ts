9cc5cea1b0fb4c1153b98dd011ecb3910a742295
CorbinV <vcorbin.wynn@gmail.com>
2025-01-04 11:45:30 +0800

    feat: 新增按键类型字符映射

--- src/enum/keyType.ts
+++ src/enum/keyType.ts
@@ -13,3 +13,17 @@ export enum KeyTypeEnum {
   RS,
   None
 }
+type EnumKeys = keyof typeof KeyTypeEnum;
+export const keyTypeEnumProxy = new Proxy(KeyTypeEnum, {
+  get(target: any, prop: string) {
+    if (prop === 'getKey') {
+      return (value: number) => {
+        const arr = Object.keys(target) as string[];
+        return arr.find(key => target[key] === value);
+      };
+    }
+    return target[prop];
+  }
+}) as typeof KeyTypeEnum & {
+  getKey(value: KeyTypeEnum): EnumKeys | undefined;
+};
