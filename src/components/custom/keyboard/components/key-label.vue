eea0b39355d4e27c50aecc4107f6ba848d95f554
CorbinV <vcorbin.wynn@gmail.com>
2025-01-04 11:46:04 +0800

    feat: 新增按键值组件

---
+++ src/components/custom/keyboard/components/key-label.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { BaseKeyView } from '@/api/modules/combo';
+
+defineProps<{
+  detail?: BaseKeyView;
+  iconClass?: string;
+  textClass?: string;
+}>();
+</script>
+
+<template>
+  <div v-if="detail?.type" class="no-wrap flex items-center justify-center text-center text-0.6rem">
+    <i v-if="detail?.type === 'icon'" class="iconfont text-10px" :class="`icon-${detail?.icon} ${iconClass}`"></i>
+    <span v-else :class="textClass">{{ detail!.label }}</span>
+  </div>
+</template>
