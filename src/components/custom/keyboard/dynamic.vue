0c0635aec3573f0f8c3a026814fe691891ff9b14
CorbinV <vcorbin.wynn@gmail.com>
2025-01-07 16:27:08 +0800

    optimize: 改键记录组件挂载

--- src/components/custom/keyboard/dynamic.vue
+++ src/components/custom/keyboard/dynamic.vue
@@ -6,6 +6,7 @@ import { $t } from '@/locales';
 import KeyControl from './components/key-control.vue';
 import LayerControl from './components/layer-control.vue';
 import Keyboard from './components/keyboard.vue';
+import RedoUndo from './components/redo-undo.vue';
 const emit = defineEmits(['change:key-id']);
 const keyboardStore = useKeyboardStore();
 const { getKeyDetail } = keyboardStore;
@@ -48,7 +49,10 @@ function handleSelectKey(data: Omit<BaseKey, 'key'> & { idx: number; keyId: stri
 
 <template>
   <div class="flex flex-row items-end justify-center gap-x-4">
-    <LayerControl v-model:layer="keyLayerInfo.layerIndex" :layer-list="layerList"></LayerControl>
+    <div class="h-full flex flex-col justify-between">
+      <RedoUndo class="mt-1/3 md:mt-12" />
+      <LayerControl v-model:layer="keyLayerInfo.layerIndex" :layer-list="layerList"></LayerControl>
+    </div>
     <Keyboard :layer="keyLayerInfo.layerIndex" class="kb-control" @update:key-id="handleSelectKey" />
     <div class="second-view flex flex-1 items-center justify-center text-2xl font-bold">
       <p>The screen is too small to display.</p>
