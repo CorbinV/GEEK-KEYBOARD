14b3582c3b64fe9d2ad56816aab1c8babc847809
CorbinV <vcorbin.wynn@gmail.com>
2025-01-18 14:57:57 +0800

    optimze: 应用层接入二进制流数据

--- src/utils/requset/deviceClient.ts
+++ src/utils/requset/deviceClient.ts
@@ -46,25 +46,58 @@ export class UsbTransfor {
     this.communicator = communicator;
   }
   request<T = any>(ops: SendOps, _cfg?: SendCfg): Promise<RespOps<T>> {
-    if (useMock) {
-      return new Promise(resolve => {
-        const data = mockData[ops.name];
-        resolve(data);
-      });
+    try {
+      if (useMock) {
+        return new Promise(resolve => {
+          const data = mockData[ops.name];
+          resolve(data);
+        });
+      }
+      // feat: use device to send data
+      return this.getCommunicator().send(ops);
+    } catch (error) {
+      console.log('error', error);
+      throw error;
     }
-    // feat: use device to send data
-    return this.getCommunicator().send(ops);
   }
   async send<T = any>(opstions: SendOps, cfg: SendCfg = { waitResponse: true }): Promise<T> {
-    // optimize: transform options and config to request
-    const sendOps = JSON.parse(JSON.stringify(opstions));
-    const { code, data } = await this.request<T>(sendOps, cfg);
-    if (code !== 0) {
-      throw new Error('error');
+    try {
+      // optimize: transform options and config to request
+      const sendOps = JSON.parse(JSON.stringify(opstions));
+      const { code, data } = await this.request<T>(sendOps, cfg);
+      if (code !== 0) {
+        throw new Error('error');
+      }
+      return data;
+    } catch (error) {
+      throw error;
     }
-    return data;
   }
-  listen() {}
+
+  requestBinary<T = any>(data: Uint8Array, cfg?: SendCfg): Promise<BinaryType> {
+    try {
+      if (useMock) {
+        // return new Promise(resolve => {
+        //   const data = mockData[ops.name];
+        //   resolve(data);
+        // });
+      }
+      return this.getCommunicator().sendBinary(data, cfg);
+    } catch (error) {
+      console.log('error', error);
+      throw error;
+    }
+  }
+
+  async sendBinary(data: Uint8Array, cfg: SendCfg = { waitResponse: true }) {
+    try {
+      const binary = await this.requestBinary<string>(data, cfg);
+      return binary;
+    } catch (error) {
+      throw error;
+    }
+  }
+  listen() { }
 }
 const requestClient = new UsbTransfor();
 export { requestClient };
