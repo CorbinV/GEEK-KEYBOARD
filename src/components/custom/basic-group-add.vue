0317c90c5cd235b573966da7924d8fe2bfa9a5b2
CorbinV <vcorbin.wynn@gmail.com>
2025-02-27 16:16:13 +0800

    style: 修复多元素场景下视图抖动问题

--- src/components/custom/basic-group-add.vue
+++ src/components/custom/basic-group-add.vue
@@ -7,9 +7,9 @@ defineProps<{
 
 <template>
   <div
-    class="min-w-120px inline-flex items-center justify-center border border-#3C8DF4 rounded-md border-dashed py-6 text-#3C8DF4 hover:cursor-pointer"
+    class="h-full min-w-120px inline-flex items-center justify-center border border-#3C8DF4 rounded-md border-dashed py-6 text-#3C8DF4 hover:cursor-pointer"
   >
-    <div class="flex flex-col justify-center gap-y-1">
+    <div class="flex flex-col justify-center gap-y-1.5">
       <i class="iconfont text-center text-2xl font-bold" :class="`icon-${icon}`"></i>
       <p class="text-base">{{ desc }}</p>
     </div>
