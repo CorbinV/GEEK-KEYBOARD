509ebbbb5544ac8f99d2bef8d5ccccae39574545
CorbinV <vcorbin.wynn@gmail.com>
2025-03-19 15:28:09 +0800

    optimize: ota 升级进度逻辑重写

--- src/views/settings/components/ota-progress.vue
+++ src/views/settings/components/ota-progress.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { computed, ref, watchEffect } from 'vue';
 import { NModal } from 'naive-ui';
+import { OtaStatusEnum } from '../composables/useOTA';
 
 const props = defineProps<{
   show: boolean;
   progress: number;
+  status: OtaStatusEnum
 }>();
 
 const emit = defineEmits(['update:show', 'handleBack']);
@@ -12,11 +14,28 @@ const emit = defineEmits(['update:show', 'handleBack']);
 const progress = ref(props.progress || 0);
 
 const showModal = computed(() => props.show);
-
+const upgradeFinished = computed(() => {
+  return props.status === OtaStatusEnum.UPGRADE_SUCCESS || props.status === OtaStatusEnum.UPGRADE_FAIL
+});
 watchEffect(() => {
   progress.value = props.progress || 0;
 });
-
+const backBtnAttr: any = computed(() => {
+  if (props.status === OtaStatusEnum.UPGRADE_SUCCESS) {
+    return {
+      class: 'flex-1 max-w-32 text-white',
+      type: 'info',
+      gohst: false,
+      size: "large",
+      onClick: handleBack
+    }
+  }
+  return {
+    class: 'flex-1 max-w-32',
+    size: "large",
+    onClick: handleBack
+  }
+})
 const handleBack = () => {
   emit('update:show', false);
 };
@@ -24,35 +43,37 @@ const handleBack = () => {
 
 <template>
   <NModal v-model:show="showModal" :mask-closable="false">
-    <div class="rounded-log flex flex-col items-center justify-center rounded-lg bg-[#171619] p-7 text-center">
-      <span class="text-4 text-[#ffffff]">固件升级</span>
-      <NProgress
-        type="line"
-        color="#3C8DF4"
-        rail-color="#D9D9D9"
-        style="width: 365px"
-        :show-indicator="false"
-        :percentage="progress"
-        class="mt-7"
-      />
-      <span class="mt-4 text-4" :class="progress > 0 ? 'text-[#3C8DF4]' : 'text-[#D9D9D9]'">{{ progress }}%</span>
-      <template v-if="progress < 100">
-        <span class="mt-5 text-4 text-[#666666]">升级过程中，请勿关闭设备以及网页</span>
-        <span class="mt-1 text-4 text-[#666666]">升级成功后会有提示，请耐心等待～</span>
-      </template>
-      <template v-else>
-        <div class="mt-5 flex">
-          <i class="iconfont icon-mouse-mid"></i>
-          <span class="ml-2 text-4 text-[#999999]">升级完成！</span>
+    <div class="rounded-log flex flex-col items-center justify-center gap-y-4 rounded-lg bg-[#171619] p-7 text-center">
+      <span class="text-4 text-[#ffffff] mb-4">{{ $t('setting.gjUpdate') }}</span>
+      <NProgress type="line" color="#3C8DF4" rail-color="#D9D9D9" style="width: 365px" :show-indicator="false"
+        :percentage="progress" class="" />
+      <span class="text-4" :class="progress > 0 ? 'text-[#3C8DF4]' : 'text-[#D9D9D9]'">{{ progress }}%</span>
+      <template v-if="upgradeFinished">
+        <div class="flex flex-row items-center gap-x-2">
+          <template v-if="status === OtaStatusEnum.UPGRADE_SUCCESS">
+            <i class="iconfont icon-hollow-done text-#a1ec41"></i>
+            <p class="text-base text-[#999999]">{{ $t('businessCommon.upgradeSuccess') }}</p>
+          </template>
+          <template v-else>
+            <i class="iconfont icon-hollow-close text-#e4583e"></i>
+            <p class="text-base text-[#999999]">{{ $t('businessCommon.upgradeFail') }}</p>
+          </template>
+        </div>
+        <div class="flex flex-row justify-center gap-x-8 w-70%">
+          <NButton v-bind="backBtnAttr">
+            {{ $t('common.back') }}
+          </NButton>
+          <NButton class="flex-1  max-w-32 text-white" size="large" type="info" @click="handleBack">
+            {{ $t('setting.upgradeAgain') }}
+          </NButton>
         </div>
       </template>
-      <button
-        v-if="progress === 100"
-        class="mt-5 h-15 w-42 rounded bg-[#3c8df4] text-4 text-[#FFFFFF]"
-        @click="handleBack"
-      >
-        返回
-      </button>
+      <template v-else>
+        <div class=" text-4 text-[#666666]">
+          <p>{{ $t('setting.upgradeNotice1') }}</p>
+          <p>{{ $t('setting.upgradeNotice2') }}</p>
+        </div>
+      </template>
     </div>
   </NModal>
 </template>
