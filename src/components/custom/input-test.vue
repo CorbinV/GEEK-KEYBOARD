c9eec53dd321a25eafe3c4decb8583e47433e438
CorbinV <vcorbin.wynn@gmail.com>
2025-05-13 15:11:54 +0800

    optimize: 按键测试展示与消失规格调整

--- src/components/custom/input-test.vue
+++ src/components/custom/input-test.vue
@@ -2,6 +2,8 @@
 import { useKeyboardStore } from '@/store/modules/keyboard';
 import { nextTick, onMounted, onUnmounted, ref, toRef } from 'vue';
 
+const MAX_KEYS = 10;
+const MAX_WAIT_TIME = 10000;
 const keyboardStore = useKeyboardStore();
 
 const kbCfg = toRef(keyboardStore, 'kbCfg');
@@ -11,11 +13,49 @@ export type KeyItem = {
   timeInterval: number;
   opacity: number;
 };
-const MAX_KEYS = 10;
+
+function useKeyQueue<T extends { id: string | number }>(maxLen: number = MAX_KEYS, timeoutMs = 3000) {
+  const queue = ref<T[]>([]);
+  let lastTimer: number | null = null;
+  const removeOutKey = (id: T['id']) => {
+    lastTimer = window.setTimeout(() => {
+      const index = queue.value.findIndex(item => item.id === id);
+      if (index !== -1) {
+        queue.value.splice(index, 1);
+      }
+    }, timeoutMs)
+  }
+  const enqueue = (item: T) => {
+    queue.value.unshift(item as any);
+    if (queue.value.length > maxLen) {
+      queue.value.pop();
+    }
+  }
+  const clearQueue = () => {
+    queue.value = [];
+  }
+
+  return {
+    queue,
+    enqueue,
+    clearQueue
+  };
+}
+
+
 const lastKeyTime = ref(0); // 用于计算时间间隔
 const hasFocus = ref(false); // 用于计算时间间隔
-// 添加一个按键输出的函数
-const addKey = (event: KeyboardEvent) => {
+const generateKey = (key: string, timeInterval: number) => {
+  return {
+    id: Date.now(),
+    key,
+    timeInterval,
+    opacity: 1 // 初始时透明度为 1（完全可见）
+  }
+}
+const { queue, enqueue, clearQueue } = useKeyQueue<KeyItem>(MAX_KEYS, 3000);
+let keyTimer: number | null = null;
+function addKey(event: KeyboardEvent) {
   event.preventDefault();
   event.stopPropagation();
   if (event.repeat) {
@@ -25,24 +65,17 @@ const addKey = (event: KeyboardEvent) => {
   const currentTime = Date.now();
   const timeInterval = lastKeyTime.value ? currentTime - lastKeyTime.value : 0;
   lastKeyTime.value = currentTime;
-  const newKey = {
-    id: Date.now(),
-    key: kbCfg.value.standardKeyMap[event.keyCode]?.label ?? 'Err',
-    timeInterval,
-    opacity: 1 // 初始时透明度为 1（完全可见）
+  const newKey = generateKey(kbCfg.value.standardKeyMap[event.keyCode]?.label ?? 'Err', timeInterval);
+  enqueue(newKey);
+  if (keyTimer) {
+    clearTimeout(keyTimer);
   }
-  if (keys.value.length > MAX_KEYS - 1) {
-    keys.value.shift()
-  }
-  keys.value.push(newKey);
 
-  setTimeout(() => {
-    newKey.opacity = 0
-    const index = keys.value.findIndex(item => item.id === newKey.id);
-    if (index !== -1) {
-      keys.value.splice(index, 1);
-    }
-  }, 3000);
+  keyTimer = window.setTimeout(() => {
+    clearQueue();
+    keyTimer = null;
+    lastKeyTime.value = 0;
+  }, MAX_WAIT_TIME);
 };
 let eventHandler: Function | undefined
 const keyContainerRef = ref<HTMLDivElement | null>(null);
@@ -58,7 +91,8 @@ function handleBlur() {
   handleClear()
 }
 function handleClear() {
-  keys.value = []
+  clearQueue();
+  lastKeyTime.value = 0;
 }
 function handleKeyDown(event: KeyboardEvent) {
   if (!hasFocus.value) {
@@ -86,16 +120,17 @@ onUnmounted(() => {
     <p v-show="!hasFocus"
       class="h-full w-full flex select-none items-center justify-center border-transparent text-[16px] text-#666666"
       :class="`${!hasFocus ? '' : '-z-1'}`" @click="testmoClick">
-      点击测试按键
+      {{ $t('baseKey.keyboard.beginKeyTest') }}
     </p>
     <div v-show="hasFocus" tabindex="-1" class="key-display-container  border border-#007bff relative"
       @blur="handleBlur" ref="keyContainerRef">
-      <div class="flex flex-row-reverse justify-end gap-x-8">
-        <template v-for="(_, index) in MAX_KEYS" :key="`${index}-${keys[index]?.key}`">
+      <div class="flex  justify-end gap-x-8">
+        <template v-for="(_, index) in MAX_KEYS" :key="`${index}-${queue[index]?.key}`">
 
-          <div v-if="keys[index]" class="key-item text-16px text-#3C8DF4  " :style="{ opacity: keys[index].opacity }">
-            <span>{{ keys[index].key + ' ' }}</span>
-            <span v-if="keys[index].timeInterval > 0">{{ (keys[index].timeInterval / 1000).toFixed(2) }}</span>
+          <div v-if="queue[index]?.key" :class="`key-item text-16px  ${index === 0 ? 'text-#3C8DF4': 'text-#999999'} `"
+            :style="{ opacity: queue[index].opacity }">
+            <span>{{ queue[index].key + ' ' }}</span>
+            <span v-if="queue[index].timeInterval > 0">{{ (queue[index].timeInterval / 1000).toFixed(2) }}</span>
           </div>
           <div v-else class="w-48px text-16px"></div>
         </template>
