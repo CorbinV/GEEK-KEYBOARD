6794ae8eae5853571d45ce991978679577b6899e
CorbinV <vcorbin.wynn@gmail.com>
2025-06-12 15:38:47 +0800

    optimise(device-transfer): 数据格式调整为 msgpack

--- packages/device-transfer/src/modules/web-hid/utils.ts
+++ packages/device-transfer/src/modules/web-hid/utils.ts
@@ -1,9 +1,9 @@
 import { arrayToJson, depacketize, objToArray, packetize } from '@sa/packetize';
-
+import { encode, decode } from '@msgpack/msgpack'
 // 消息编解码器
 export class HIDMessageCodec {
   encodeMessage(_: string, data: any): Uint8Array[] {
-    const sendBuf = objToArray(data);
+    const sendBuf = encode(data);
     const packageList = packetize(sendBuf as Uint8Array, 64);
     return packageList;
   }
@@ -15,10 +15,10 @@ export class HIDMessageCodec {
   }
   decodeMessage(data: DataView): any {
     const contentBuf = depacketize(new Uint8Array(data.buffer));
-    const jsonStr = arrayToJson(contentBuf || new Uint8Array());
+    const jsonData = decode(contentBuf || new Uint8Array());
     try {
-      if (contentBuf && jsonStr) {
-        return JSON.parse(jsonStr);
+      if (contentBuf && jsonData) {
+        return jsonData;
       }
       return {};
     } catch (error) {
