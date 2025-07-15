6a420899760fb0f18f41087b5422f2db741e8e13
CorbinV <vcorbin.wynn@gmail.com>
2025-07-15 11:46:52 +0800

    optimize: 适配 ota 包更新

--- src/hooks/business/useDeviceOta.ts
+++ src/hooks/business/useDeviceOta.ts
@@ -3,7 +3,7 @@ import { useDeviceStore } from '@/store/modules/device';
 const store = useDeviceStore();
 const requestClient = store.getDeviceClient();
 export function useDeviceOta() {
-  const otaInstance = createOTA(OTA_TYPE.WCH, {
+  const otaInstance = createOTA(OTA_TYPE.WCH_585, {
     outMtu: 64,
     sendFnc: requestClient.sendBinary.bind(requestClient) as any
   });
