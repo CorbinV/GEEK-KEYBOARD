d7e5d5f5afa28989c3fd33278cde13169e748d98
CorbinV <vcorbin.wynn@gmail.com>
2025-07-15 11:51:57 +0800

    feat: 新增配置相关接口

---
+++ src/api/modules/setting.ts
@@ -0,0 +1,28 @@
+export enum DeviceType {
+  GAMEPAD,
+  TRANSPORT,
+  KEYBOARD
+}
+export type DeviceVersion = {
+  id: number;
+  ver: number;
+  tp: number;
+  mode: string;
+};
+export type DeviceInfo = {
+  aVer: number;
+  rate: number;
+  devTp: DeviceType;
+  devVer: DeviceVersion[];
+};
+export enum DeviceIptEnum {
+  PC,
+  PS,
+  SWITCH
+}
+export type DeviceIpt = {
+  ipt: DeviceIptEnum;
+};
+export type Rate = {
+  rate: number;
+};
---
+++ src/api/setting.ts
@@ -0,0 +1,18 @@
+import requestClient from './config';
+import type { DeviceInfo, DeviceIpt, Rate } from './modules/setting';
+
+export function getDeviceInfo() {
+  return requestClient.send<DeviceInfo>({
+    name: 'gDevInfo'
+  });
+}
+export function getRate() {
+  return requestClient.send<Rate>({
+    name: 'gRate'
+  });
+}
+export function getInputType() {
+  return requestClient.send<DeviceIpt>({
+    name: 'gIptTp'
+  });
+}
