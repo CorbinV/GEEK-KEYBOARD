7facaaf9477068034f91606826796d93078be545
CorbinV <vcorbin.wynn@gmail.com>
2025-07-16 16:43:31 +0800

    feat: socd 组件仅在非赛事层展示

--- src/views/base-key/index.vue
+++ src/views/base-key/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, toRefs } from 'vue';
+import { useKeyboardStore } from '@/store/modules/keyboard';
 import SOCD from './module/socd.vue';
 import Control from './module/control.vue';
 import BtnList from './module/btn-list.vue';
@@ -7,12 +8,20 @@ const btnName = ref('');
 function injBtnItemClick(k: string) {
   btnName.value = k;
 }
+
+const keyboardStore = useKeyboardStore();
+const { keyLayerInfo } = toRefs(keyboardStore);
+const showSocd = computed(() => {
+  return !(keyLayerInfo.value.layerIndex === 0);
+});
 </script>
 
 <template>
   <div class="flex flex-col p-6 pt-17">
     <div class="grid grid-cols-5">
-      <SOCD class="col-span-1"></SOCD>
+      <div class="col-span-1">
+        <SOCD v-show="showSocd"></SOCD>
+      </div>
       <div class="col-span-3">
         <Control :update-btn="btnName"></Control>
       </div>
