5368f8daa3976cbb176ab53161f2ab11ac781510
CorbinV <vcorbin.wynn@gmail.com>
2025-07-26 10:12:05 +0800

    refactor: 移除无效代码

--- src/components/custom/keyboard/index.ts
+++
@@ -1,6 +0,0 @@
-import DynamicKeyboard from './dynamic.vue';
-import StandardKeyboard from './standard.vue';
-import KeyboardContainer from './container.vue';
-import BaseKeyboard from './components/keyboard.vue';
-export { DynamicKeyboard, StandardKeyboard, KeyboardContainer, BaseKeyboard };
-export default KeyboardContainer;
--- src/views/super-key/config/index.ts
+++
@@ -1,5 +0,0 @@
-export const SOCDTriggerOps = [
-  { label: '优先最后按下的按键', value: 0 },
-  { label: '优先1号位触发', value: 1 },
-  { label: '优先2号位触发', value: 2 }
-];
